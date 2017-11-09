60453f9af0e0f099e101d03627a5a0a1beed99cd
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-11-09 12:22:55 -0400

    Add reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,16 @@
+export default function(){
+  return [
+    {
+      title: 'Javascript the good parts', pages: 101
+    },
+    {
+      title: 'Django', pages: 102
+    },
+    {
+      title: 'React', pages: 1055
+    },
+    {
+      title: 'Halo', pages: 300
+    }
+  ]
+}
